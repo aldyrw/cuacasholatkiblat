1d9e1b6b97b48a935ae56f27cf79d2cec7f94d1d
aldyrw <32488529+aldyrw@users.noreply.github.com>
2017-12-21 21:16:44 +0700

    Add files via upload

---
+++ bot2.php
@@ -0,0 +1,113 @@
+<?php
+/*
+copyright @ medantechno.com
+Modified @ ARW
+2017
+
+*/
+
+require_once('./line_class.php');
+require_once('./unirest-php-master/src/Unirest.php');
+
+$channelAccessToken = 'rxIFoRfTZ1vXvs3Ipu+ejN6Vwq+iKNIQvPEV2I4gGHQ0X8OAl8L3eHa7VqF2jOZOHEiBUfVd8ZMqn9T6QsV7LmZFp94vmUOTnat0aE/68+4JYKCSqSGYxEvvDGedA/QMuCRbXubQ2HUDa41u9+wSkgdB04t89/1O/w1cDnyilFU='; //sesuaikan 
+$channelSecret = '0ec5f958a78169b8f9b569c23c162ae8';//sesuaikan
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+
+$userId 	= $client->parseEvents()[0]['source']['userId'];
+$groupId 	= $client->parseEvents()[0]['source']['groupId'];
+$replyToken = $client->parseEvents()[0]['replyToken'];
+$timestamp	= $client->parseEvents()[0]['timestamp'];
+$type 		= $client->parseEvents()[0]['type'];
+
+$message 	= $client->parseEvents()[0]['message'];
+$messageid 	= $client->parseEvents()[0]['message']['id'];
+
+$profil = $client->profil($userId);
+
+$pesan_datang = explode(" ", $message['text']);
+
+$command = $pesan_datang[0];
+$options = $pesan_datang[1];
+if (count($pesan_datang) > 2) {
+    for ($i = 2; $i < count($pesan_datang); $i++) {
+        $options .= '+';
+        $options .= $pesan_datang[$i];
+    }
+}
+
+#-------------------------[Function]-------------------------#
+function kiblat($keyword) {
+    $uri = "https://time.siswadi.com/qibla/" . $keyword;
+
+    $response = Unirest\Request::get("$uri");
+
+    $json = json_decode($response->raw_body, true);
+    $result = "Arah Kiblat ";
+	$result .= $json['location']['address'];
+	$result .= "\nArah : ";
+	$result .= $json['time']['image'];
+    return $result;
+}
+#-------------------------[Function]-------------------------#
+
+# require_once('./src/function/search-1.php');
+# require_once('./src/function/download.php');
+# require_once('./src/function/random.php');
+# require_once('./src/function/search-2.php');
+# require_once('./src/function/hard.php');
+
+//show menu, saat join dan command /menu
+if ($type == 'join' || $command == '/menu') {
+    $text = "Ini adalah Dora - Bot iseng buatan ARW";
+    $balas = array(
+        'replyToken' => $replyToken,
+        'messages' => array(
+            array(
+                'type' => 'text',
+                'text' => $text
+            )
+        )
+    );
+}
+
+	//pesan bergambar
+if($message['type']=='text') {
+	    if ($command == '/kiblat') {
+
+        $result = kiblat($options);
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => $result
+                )
+            )
+        );
+    }
+
+}else if($message['type']=='sticker')
+{	
+	$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',									
+										'text' => 'Makasih Kak Stikernya ^_^'										
+									
+									)
+							)
+						);
+						
+}
+if (isset($balas)) {
+    $result = json_encode($balas);
+//$result = ob_get_clean();
+
+    file_put_contents('./balasan.json', $result);
+
+
+    $client->replyMessage($balas);
+}
+?>
